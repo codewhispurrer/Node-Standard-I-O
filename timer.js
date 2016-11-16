abfbe12e1c8e32297be35b5c24b23848fb7a42e0
Sherry Umlah <sumlah@articulate.com>
2016-11-16 11:24:44 -0600

    Update timer with lower-case module

--- timer.js
+++ timer.js
@@ -1,23 +1,32 @@
-// README: Example of standard i/o process with interval timer in Node
-// Execute within the terminal:
-// $ node timer
+var lowerCase = require('lower-case')
 
 var question = "Set timer for how many minutes? ";
 process.stdout.write(`${question}`);
 
 // Event listener for standard input process
 process.stdin.on('data', function(data){
+
   if (isNaN(data)){
-    // Ensure input a number
-    question = "\nThat is not a number. Set timer for how many minutes? "
-    process.stdout.write(`${question}`);
+    data = lowerCase(data);
+    data = data.trim();
+    if (data === "yes"){
+      question = "Set timer for how many minutes? ";
+      process.stdout.write(`${question}`);
+    } else if (data === "no"){
+      process.stdout.write("\n\nOK. Goodbye\n\n");
+      process.exit();
+    } else {
+      // Ensure input is a number
+      question = "\nThat is not a number. Want to set a timer? Enter 'yes' or 'no': "
+      process.stdout.write(`${question}`);
+    };
   } else {
     var seconds = (data*60)*1000;
     var waitInterval = 1000; // one second
-    setInterval(function(){
+    var interval = setInterval(function(){
       if (seconds <= 0){
-        process.stdout.write("Time is up.");
-        process.exit();
+        clearInterval(interval);
+        process.stdout.write("Time is up.\nWant to set another timer? Enter 'yes' or 'no': ");
       } else {
         seconds -= waitInterval;
         console.log(`${seconds/1000} seconds left...`);
